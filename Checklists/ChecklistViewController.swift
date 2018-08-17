bed31bccc6257bd10207ba0bbe7f9e6f73791b87
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2018-08-16 21:46:25 -0300

    Segundo comia

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -14,15 +14,31 @@ class ChecklistViewController: UITableViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        return cell
+    }
 
+    
 }
+
+
+
+
+
+
+
+
 
