36ea5e51da92ba25dca16ff1bf51a61f442994d5
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2018-08-21 23:46:17 -0300

    commit 6

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,34 +8,77 @@
 
 import UIKit
 
+protocol AddItemViewControllerDelegate: class
+{
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController)
+    func addItemViewController(_ controller: AddItemViewController, didFinishedAdding item: ChecklistItem)
+}
+
 class AddItemViewController: UITableViewController, UITextFieldDelegate
 {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    
+    weak var delegate: AddItemViewController?
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
-        textField.delegate = self
+        //textField.delegate = self
     }
     
+    /*func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return false
+    }
+    */
+    // Fecha a view
     @IBAction func cancel(_ sender: UIBarButtonItem)
+    {
+        navigationController?.popViewController(animated: true)
+        //delegate?.addItemViewControllerDidCancel(self)
+    }
+    @IBAction func endEdit(_ sender: UITextField)
     {
         navigationController?.popViewController(animated: true)
     }
     
+    // Fecha a view
     @IBAction func done(_ sender: UIBarButtonItem)
     {
         navigationController?.popViewController(animated: true)
     }
     
-
-
+    // Evita selecao da linha
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?
+    {
+        return nil
+    }
+    
     // Seta o campode texto como FR
     override func viewWillAppear(_ animated: Bool)
     {
         textField.becomeFirstResponder()
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)
+        let newText = oldText.replacingCharacters(in: stringRange!, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    /*func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+
+    }
+*/
 }
